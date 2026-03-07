830ed4db68dccfa23deacf1a8865ce4809ebbf0d
Angeles <karolso147@gmail.com>
2026-03-06 21:55:13 -0500

    restore spotify callback page

---
+++ app/callback/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+
+export default function CallbackPage() {
+
+  const router = useRouter();
+  const params = useSearchParams();
+
+  useEffect(() => {
+
+    const code = params.get("code");
+
+    if (!code) {
+      router.push("/");
+      return;
+    }
+
+    console.log("Spotify code:", code);
+
+    router.push("/profile");
+
+  }, []);
+
+  return (
+    <div className="min-h-screen flex items-center justify-center text-white">
+      Conectando con Spotify...
+    </div>
+  );
+
+}
